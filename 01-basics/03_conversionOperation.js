713399d4b7a7335f0d0aa3cd3b1e47d5db905361
VrajSoni <vrajsoni624@gmail.com>
2026-05-12 20:28:46 +0530

    coversion and comparision of datatypes

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,51 @@
+// let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+
+/**
+"33" => 33
+"33abc" => NaN
+null => 0
+true => 1 ; false => 0
+undefined => NaN
+ */
+
+
+
+
+let isLogIn = " "
+let booleanIsLogIn = Boolean(isLogIn)
+// console.log(booleanIsLogIn);
+
+// 1 => true ; 0 => false
+// "" => false ;  "vraj" => true
+// ** " " => true **
+
+
+
+
+// ******************************** Operations ***********************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+let str1 = "hello"
+let str2 = " vraj"
+let str3 = str1 + str2
+console.log(str3);
---
+++ 01-basics/04_comparision.js
@@ -0,0 +1,11 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1);
+
+// clean code is most valueable 

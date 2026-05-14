d6ff974254c752f9fe8ddb748cf79b9b437a2628
VrajSoni <vrajsoni624@gmail.com>
2026-05-14 19:59:33 +0530

    date and time in javascript

---
+++ 01-basics/07_dates.js
@@ -0,0 +1,12 @@
+let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+let myNewDate = new Date(2004, 4, 20, 9, 15)
+console.log(myNewDate.toLocaleString());
+
+
+// dates and time is not so complex in this AI era 
+// jab samaj naa aye use AI to understand rather than skiping this topic its 
+// better to learn with tech and AI.

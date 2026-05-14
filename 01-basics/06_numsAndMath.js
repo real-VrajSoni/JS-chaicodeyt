f00c8bd6be4e222829dacd7f48136a5d8d780611
VrajSoni <vrajsoni624@gmail.com>
2026-05-14 19:20:17 +0530

    nums and math

---
+++ 01-basics/06_numsAndMath.js
@@ -0,0 +1,34 @@
+const score = 400
+//console.log(score);
+
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toFixed(2));
+//console.log(balance.toString().length);
+
+const otherNumber = 123.356
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// ++++++++++++++++++++++++++++++ MATH +++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-6));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.2));
+// console.log(Math.ceil(4.2));
+
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min =10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+

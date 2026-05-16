7b5213b57a6a037edde156e204d8234095700077
VrajSoni <vrajsoni624@gmail.com>
2026-05-16 20:38:07 +0530

    more functions

--- 03-basics3/01_functions.js
+++ 03-basics3/01_functions.js
@@ -30,4 +30,37 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("vraj"))
+// console.log(loginUserMessage("vraj"))
+
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500));
+
+
+const user = {
+    username : "vraj",
+    prices : 199,
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+// handleObject(user)
+handleObject({
+    username : "sam altman",
+    price : 399
+})
+
+
+const myNewArr = [200, 400, 300, 500]
+
+function returnArray(getArray){
+    return getArray[1]
+}
+
+console.log(returnArray([100,200,300.400]));

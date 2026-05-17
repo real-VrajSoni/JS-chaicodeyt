56316473c186b3ae7962e127ac0508044d8446b6
VrajSoni <vrajsoni624@gmail.com>
2026-05-17 12:58:21 +0530

    scopes in JS

--- 03-basics3/02_scope.js
+++ 03-basics3/02_scope.js
@@ -11,3 +11,32 @@ if (true) {
 // console.log(c);
 
 // do not use VAR for decalaring variables
+
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+
+
+
+addOne(5)
+function addOne(num){
+    return num + 1
+}
+
+
+const addTwo = function(num){
+    return num + 1
+}
+addTwo(5)

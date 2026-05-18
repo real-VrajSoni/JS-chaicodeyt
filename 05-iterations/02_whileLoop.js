f28245d6cdee853cbe5c7b0169c940848494b7da
VrajSoni <vrajsoni624@gmail.com>
2026-05-18 21:28:38 +0530

    while loop in JS

---
+++ 05-iterations/02_whileLoop.js
@@ -0,0 +1,27 @@
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index +2 
+    
+}
+
+//////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/\/\/\\//\/\\\//\/\\
+
+let myArray = ["ironman", "thor" , "superman"]
+let arr = 0
+
+while (arr < myArray.length) {
+    console.log(myArray[arr]);
+    arr += 1
+    
+}
+/////////////////////////////////////////////////////////
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score ++
+} while (score <= 10);

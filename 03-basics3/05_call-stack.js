42c76a8cc485c679626e4485cc85a56aaf70c078
VrajSoni <vrajsoni624@gmail.com>
2026-05-18 12:48:27 +0530

    call stack in JS

---
+++ 03-basics3/05_call-stack.js
@@ -0,0 +1,19 @@
+// call stack and JS execution context
+
+/*
+JavaScript Execution Context 
+
+when we write code the global EC is created which is referred as (this)
+
+3 type of EC:
+1. global Ec
+2. function EC
+3. eval EC
+
+
+{} => memory creation phase => memory allocate hoti hai
+{} => execution phase  => execute hota hai
+
+
+
+*/

545f557496f80ee21a477191d0a4307b94414d6e
VrajSoni <vrajsoni624@gmail.com>
2026-05-24 20:52:35 +0530

    project async

---
+++ 07-events/async.js
@@ -0,0 +1,9 @@
+/**
+  default JS is synchronous and single threaded 
+
+  //each JS operations waits for the last one to complete before executing
+
+    blocking code                    vs          non-blocking code
+    block the flow of program                   does not block
+    read file sync                                   read file async
+ */

e76fdfcf7ce88d1f0c3577bbc07615ca24bf27f1
VrajSoni <vrajsoni624@gmail.com>
2026-05-29 18:48:14 +0530

    oop in JS

--- 09-OOPs/oop.js
+++ 09-OOPs/oop.js
@@ -7,5 +7,27 @@ const user = {
     console.log(`Username: ${this.username}`);
   },
 };
-console.log(user.username);
-console.log(user.getUserDetails());
+// console.log(user.username);
+// console.log(user.getUserDetails());
+
+//******************************************************************* */
+
+// const promiseOne = new Promise()
+
+function User(username, loginCount, isLoggedIn) {
+  // myUserName = username
+  this.username = username;
+  this.loginCount = loginCount;
+  this.isLoggedIn = isLoggedIn;
+
+  this.greeting = function () {
+    console.log(`Welcome ${this.username}`);
+  };
+
+  return this;
+}
+
+const userOne = new User("vraj", 8, true);
+const userTwo = new User("chaicode", 11, false);
+console.log(userOne.constructor);
+// console.log(userTwo);

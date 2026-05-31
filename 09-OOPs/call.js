d71e916bf44e325b687a01d77bf8258470740a71
VrajSoni <vrajsoni624@gmail.com>
2026-05-31 19:12:46 +0530

    call in js

---
+++ 09-OOPs/call.js
@@ -0,0 +1,15 @@
+function SetUserName(username) {
+    //complex db calls
+    this.username = username;
+    console.log("called");
+}
+
+function createUser(username, email, password) {
+    SetUserName.call(this, username);
+
+    this.email = email;
+    this.password = password;
+}
+
+const chai = new createUser("chai", "chai@meta.com", "123");
+console.log(chai);

37cb4cca6a81fe636f8c9e23129685567c58851d
VrajSoni <vrajsoni624@gmail.com>
2026-06-03 20:36:11 +0530

    advance object

---
+++ 10-classes/mathpy.js
@@ -0,0 +1,33 @@
+// const descripter = Object.getOwnPropertyDescriptor(Math, "PI");
+// console.log(descripter);
+
+// console.log(Math.PI);
+
+// const myNewObject = Object.create(null)
+
+const chai = {
+    name: "ginger chai",
+    price: 250,
+    isAvailable: true,
+
+    order: function () {
+        console.log("chai nahi bni");
+    },
+};
+
+// console.log(chai);
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, "name", {
+    writable: false,
+    enumerable: false,
+});
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== "function") {
+        console.log(`${key} : ${value}`);
+    }
+}
